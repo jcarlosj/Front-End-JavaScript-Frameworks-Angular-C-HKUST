acf418959d4203e9d8cee718b08561887774ea39
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-17 13:01:09 -0500

    [+] ng: Generate scaffolding for the Dish-Detail Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,13 @@ import { MatButtonModule } from '@angular/material/button';
 
 /** Components */
 import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishDetailComponent
   ],
   imports: [
     BrowserModule,
