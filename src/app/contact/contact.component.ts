ab534ff54fa196a4da1af2dd551b2da2a6efda13
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-23 17:13:05 -0500

    [+] Component > contact: Create HTML structure

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+/** Dependencies */
+import { faPhone, faFax, faEnvelope as faEnvelopeS } from '@fortawesome/free-solid-svg-icons';
+import { faSkype } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  /** Icons */
+  faPhone = faPhone;
+  faFax = faFax;
+  faEnvelopeS = faEnvelopeS;
+  faSkype = faSkype;
+  faEnvelope = faEnvelope;
 
   constructor() { }
 
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -4,8 +4,6 @@ import { Component, OnInit } from '@angular/core';
 import { faPhone, faFax, faEnvelope as faEnvelopeS } from '@fortawesome/free-solid-svg-icons';
 import { faGooglePlusG, faFacebookF, faLinkedinIn, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
