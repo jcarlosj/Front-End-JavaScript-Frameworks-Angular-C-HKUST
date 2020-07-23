948d37b210223adb285b9c1f6fe84d30e637b4c9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-23 18:11:26 -0500

    [+] Component > Home: 1. Update Dish Service 2. Create HTML structure to display data from the Dish Service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+/** Models */
+import { Dish } from '../shared/Dish';
+
+/** Services */
+import { DishService } from '../services/dish.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    /** Atributes */
+    dish: Dish;
 
-  constructor() { }
+    constructor( private dishservice: DishService ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+        this .dish = this .dishservice .getFeaturedDish();
+    }
 
 }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -12,8 +12,16 @@ import { DISHES } from '../shared/dishes';
 export class DishService {
 
   constructor() { }
-
+    /** Get all the dishes */
     getDishes(): Dish[] {
-      return DISHES;
+        return DISHES;
+    }
+    /** Get dish by ID */
+    getDish( id: string ): Dish {
+        return DISHES .filter( ( dish ) => ( dish .id === id ) ) [ 0 ];
+    }
+    /** Get only featured dishes */
+    getFeaturedDish(): Dish {
+        return DISHES .filter( ( dish ) => dish .featured ) [ 0 ];
     }
 }
