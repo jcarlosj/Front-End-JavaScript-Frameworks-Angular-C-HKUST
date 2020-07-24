b94ad3c0270df2d892cb2bd802995670368d893f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-07-24 11:10:48 -0500

    [+] Service > promotion: 1. Inject Service to the Home Component 2. Using Service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 /** Models */
 import { Dish } from '../shared/Dish';
+import { Promotion } from '../shared/Promotion';
 
 /** Services */
 import { DishService } from '../services/dish.service';
+import { PromotionService } from '../services/promotion.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,11 +16,16 @@ import { DishService } from '../services/dish.service';
 export class HomeComponent implements OnInit {
     /** Atributes */
     dish: Dish;
+    promotion: Promotion;
 
-    constructor( private dishservice: DishService ) { }
+    constructor(
+        private dishService: DishService,
+        private promotionService: PromotionService
+    ) { }
 
     ngOnInit() {
-        this .dish = this .dishservice .getFeaturedDish();
+        this .dish = this .dishService .getFeaturedDish();
+        this .promotion = this .promotionService .getFeaturedPromotion();
     }
 
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+/** Models */
+import { Promotion } from '../shared/Promotion';
+
+/** Static Data */
+import { PROMOTIONS } from '../shared/promotions';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PromotionService {
 
-  constructor() { }
+    constructor() { }
+
+    /** Get all the promotions */
+    getPromotions(): Promotion[] {
+        return PROMOTIONS;
+    }
+    /** Get promotion by ID */
+    getPromotion( id: string ): Promotion {
+        return PROMOTIONS .filter( ( promo ) => ( promo .id === id ) ) [ 0 ];
+    }
+    /** Get only featured dishes */
+    getFeaturedPromotion(): Promotion {
+        return PROMOTIONS .filter( ( promotion ) => promotion .featured ) [ 0 ];
+    }
+
 }
