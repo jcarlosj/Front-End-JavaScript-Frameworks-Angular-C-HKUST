75180ec7da1122e18823cf99eb6c2dce3e700fc6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-01 13:43:48 -0500

    [$] Service > Dish: Simulating Time Delay within the Service

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -14,14 +14,23 @@ export class DishService {
   constructor() { }
     /** Get all the dishes */
     getDishes(): Promise<Dish[]> {
-        return Promise .resolve( DISHES );
+        return new Promise( resolve => {
+            // Simulate server latency with 2 second delay
+            setTimeout( () => resolve( DISHES ), 2000 );
+        });
     }
     /** Get dish by ID */
     getDish( id: string ): Promise<Dish> {
-        return Promise .resolve( DISHES .filter( ( dish ) => ( dish .id === id ) ) [ 0 ] );
+        return new Promise( resolve=> {
+            // Simulate server latency with 2 second delay
+            setTimeout( () => resolve( DISHES .filter( ( dish ) => ( dish .id === id ) ) [ 0 ] ), 2000 );
+        });
     }
     /** Get only featured dishes */
     getFeaturedDish(): Promise<Dish> {
-        return Promise .resolve( DISHES .filter( ( dish ) => dish .featured ) [ 0 ] );
+        return new Promise( resolve => {
+            // Simulate server latency with 2 second delay
+            setTimeout( () => { resolve( DISHES .filter( ( dish ) => dish .featured ) [ 0 ] ) }, 2000 );
+        });
     }
 }
