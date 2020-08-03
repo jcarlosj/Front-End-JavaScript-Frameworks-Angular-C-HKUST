66930b1354eddc0bba7b5fa3dfad692958b487b6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-03 11:42:51 -0500

    [+] Component > Contact: 1. Validate reactive form from code using observables 2. Show validation error messages to users in your view

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -27,6 +27,36 @@ export class ContactComponent implements OnInit {
     feedBack: Feedback;
 
     contactType = ContactType;
+    // It will contain the error messages to display for each field of the form defined here
+    formErrors = {
+        'firstName': '',
+        'lastName': '',
+        'phoneNumber': '',
+        'email': ''
+    };
+    // It will contain the messages for each type of expected error per form field
+    validationsMessages = {
+        'firstName': {
+            'required': 'First name is required',
+            'minlength': 'First name must be at least 3 characters long',
+            'maxlength': 'First name cannot be more than 25 characters'
+        },
+        'lastName': {
+            'required': 'Last name is required',
+            'minlength': 'Last name must be at least 3 characters long',
+            'maxlength': 'Last name cannot be more than 25 characters'
+        },
+        'phoneNumber': {
+            'required': 'Phone number is required',
+            'minlength': 'Minimum length is 7 digits (in the case of landlines)',
+            'maxlength': 'Maximum length is 10 digits (in the case of mobile phones)',
+            'pattern': 'Phone number must be contain only numbers'
+        },
+        'email': {
+            'required': 'E-mail is required',
+            'email': 'Invalid E-mail'
+        }
+    };
 
     @ViewChild( 'contactForm' ) feedBackFormDirective;
 
@@ -38,17 +68,76 @@ export class ContactComponent implements OnInit {
     }
 
     createForm() {
-        /** Define State Form */
+        /** Define State Form (Add Form Validation) */
         this .feedBackForm = this .fb .group({
-            firstName: [ '', Validators .required ],
-            lastName: [ '', Validators .required ],
-            phoneNumber: [ 0, Validators .required ],
-            email: [ '', Validators .required ],
+            firstName: [ '', [
+                Validators .required,
+                Validators .minLength( 3 ),
+                Validators .maxLength( 25 )
+            ] ],
+            lastName: [ '', [
+                Validators .required,
+                Validators .minLength( 3 ),
+                Validators .maxLength( 25 )
+            ] ],
+            phoneNumber: [ 0, [
+                Validators .required,
+                Validators .minLength( 7 ),
+                Validators .maxLength( 10 ),
+                Validators .pattern
+            ] ],
+            email: [ '', [
+                Validators .required,
+                Validators .email
+            ] ],
             agree: false,
             contactType: '',
             message: '',
             contacttype: 'None'
         });
+
+        /** Observable: Subscribe to the Angular Form observable named valueChanges */
+        this .feedBackForm .valueChanges
+             .subscribe( data => this .onValueChanged( data ) );
+
+        this .onValueChanged();     // Reset Form Validation Messages
+    }
+
+
+    /** Method that validates when the value of a form field has changed  */
+    onValueChanged( data?: any ) {
+        // Validate if feedBackForm has NOT been created
+        if( !this .feedBackForm ) {
+            return;
+        }
+
+        const form = this .feedBackForm;
+        // console .log( 'this.feedBackForm', form );
+
+        // Loops through form fields defined in formErrors
+        for( const field in this .formErrors ) {
+            if( this .formErrors .hasOwnProperty( field ) ) {
+                // Clear previous error message (if any)
+                this .formErrors[ field ] = '';
+
+                const control = form .get( field );
+                // console .log( 'control', control );
+
+                // Validate if it has a value, if the value of the field has changed, if the value is not valid
+                if( control && control .dirty && !control .valid ) {
+                    const messages = this .validationsMessages[ field ];     // Assign error message to the respective field
+                    console .log( 'messages', messages );
+
+                    for( const key in control .errors ) {
+                        if( control .errors .hasOwnProperty( key ) ) {
+                            this .formErrors[ field ] = messages[ key ] + ' ';
+                        }
+                    }
+                }
+            }
+        }
+
+        console .log( 'this.formErrors', this .formErrors );
     }
 
     onSubmit() {
