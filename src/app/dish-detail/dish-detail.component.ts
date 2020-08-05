85f8e35b56616d4b9af49f1a2071a5e57165a1b3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-05 14:22:57 -0500

    [+] Task 2: 1. Display error messages by form field. 2. Enable form button

--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -76,6 +76,48 @@ export class DishDetailComponent implements OnInit {
             date: '',
             ranking: 0
         });
+
+        /** Observable: Subscribe to the Angular Form observable named valueChanges */
+        this .dishCommentForm .valueChanges
+             .subscribe( data => this .onValueChanged( data ) );
+
+             this .onValueChanged();     // Reset Form Validation Messages
+    }
+
+    /** Method that validates when the value of a form field has changed  */
+    onValueChanged( data?: any ) {
+        // Validate if feedBackForm has NOT been created
+        if( !this .dishCommentForm ) {
+            return;
+        }
+
+        const form = this .dishCommentForm;
+        // console .log( 'this.feedBackForm', form );
+
+        // Loops through form fields defined in formErrors
+        for( const field in this .formErrors ) {
+            if( this .formErrors .hasOwnProperty( field ) ) {
+                // Clear previous error message (if any)
+                this .formErrors[ field ] = '';
+
+                const control = form .get( field );
+                // console .log( 'control', control );
+
+                // Validate if it has a value, if the value of the field has changed, if the value is not valid
+                if( control && control .dirty && !control .valid ) {
+                    const messages = this .validationsMessages[ field ];     // Assign error message to the respective field
+                    console .log( 'messages', messages );
+
+                    for( const key in control .errors ) {
+                        if( control .errors .hasOwnProperty( key ) ) {
+                            this .formErrors[ field ] = messages[ key ] + ' ';
+                        }
+                    }
+                }
+            }
+        }
+
+        console .log( 'this.formErrors', this .formErrors );
     }
 
     ngOnInit(): void {
