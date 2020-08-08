fbe4afb892a200608afe8e5dcbe9fdef83b44cbb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-08 17:23:58 -0500

    [+] json-server: 1. Set up and start a server 2. Set a public directory to serve images

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const
+    data = require( './data/dbs' ),
+    path = require( 'path' ),
+    express = require( 'express' ),
+    jsonServer = require( 'json-server' ),
+    server = jsonServer .create(),
+    router = jsonServer .router( data() ),
+    //middlewares = jsonServer .defaults(),
+    port = 5000;
+
+server .use( express .static( path .join( __dirname, 'public' ) ) );
+//server .use( middlewares );
+server .use( router );
+
+server .listen( port, () => {
+    console .log( `JSON Server is running at port ${ port }` );
+});
