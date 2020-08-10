5974d6e63a65ce21ca20259323936463f9249627
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-10 11:55:03 -0500

    [+] json-server: Some adjustments

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ const
     jsonServer = require( 'json-server' ),
     server = jsonServer .create(),
     router = jsonServer .router( data() ),
-    //middlewares = jsonServer .defaults(),
+    middlewares = jsonServer .defaults(),
     cors = require( 'cors' ),
     port = 5000;      // Important configure the same port
 
 server .use( cors() );
 server .use( express .static( path .join( __dirname, 'public' ) ) );
-//server .use( middlewares );
+server .use( middlewares );
 server .use( router );
 
 server .listen( port, () => {
