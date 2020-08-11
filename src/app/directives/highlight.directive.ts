3930805cc25fbc664c0506fbb5002df509ecd39e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-11 09:48:21 -0500

    [+] Directive > HighLight: 1. Define functionality of the directive to perform a DOM element 2. Define CSS Class 3. Implement Directive in the Menu Component

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener  } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+    constructor(
+        private el: ElementRef,         // Use this API as the last resort when direct access to DOM is needed.
+        private renderer: Renderer2     // By default, Angular renders a template into DOM.
+    ) { }
+
+    /** Listen Event 'mouseenter' */
+    @HostListener( 'mouseenter' ) onMouseEnter() {
+      this .renderer .addClass( this .el .nativeElement, 'highlight' );
+    }
+    /** Listen Event 'mouseleave' */
+    @HostListener( 'mouseleave' ) onMouseLeave() {
+      this .renderer .removeClass( this .el .nativeElement, 'highlight' );
+    }
 
 }
