fcfc6c571f49112b75b55c85179167d92b241e61
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-12 12:11:16 -0500

    [+] Service > Feedback: Define method for POST request to the server and insert a new record

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+/** Base URL - BackEnd Server (It is a recommended practice) */
+import { BASE_URL } from '../shared/baseurl';
+
+/** ReactiveX Libraries */
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+/** Models */
+import { Feedback } from '../shared/Feedback';
+
+/** Services */
+import { ProcessHttpMessageService } from '../services/process-http-message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
 
-  constructor() { }
+    constructor(
+        private http: HttpClient,
+        private processHttpMessageService: ProcessHttpMessageService
+    ) { }
+
+    /** Insert a Feedback */
+    postFeedback( feedback: Feedback ): Observable< Feedback > {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            })
+        };
+
+        return this .http .post< Feedback >( `${ BASE_URL }feedback`, feedback, httpOptions );
+    }
+
 }
